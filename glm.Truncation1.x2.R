96d7bd39f5ff86e54566defd64bd2c0db60dfae7
Mark C. Bitter <mbitter23@gmail.com>
2024-10-31 07:13:58 -0700

    Add files via upload
    uploaded project code

---
+++ glm.Truncation1.x2.R
@@ -0,0 +1,30 @@
+#GLM on all cages throughout expansion
+source("/home/users/mcbitter/OrchardProject/Code/config.R")
+source("/home/users/mcbitter/OrchardProject/Code/helper_functions.R")
+source("/home/users/mcbitter/OrchardProject/Code/load_packages.R")
+source("/home/users/mcbitter/OrchardProject/Code/plotting_functions.R")
+source("/home/users/mcbitter/OrchardProject/Code/workflow_functions.R")
+source("/home/users/mcbitter/OrchardProject/Code/general_cage_functions.R")
+source("/home/users/mcbitter/OrchardProject/Code/IndoorCageExp/indoor.cage.functions.R")
+
+setwd('~/dpetrov/MarkB/IndoorCageExp/')
+
+vec = c(0:21)
+load(paste0('./RData/indoor_cages_filteredx2.RData'))
+df.glm = sites
+samps = samps %>% mutate(tpt = as.numeric(as.character(tpt)))
+afmat = cbind(samps, t(afmat))
+eec = cbind(samps, t(eec))
+afmat = as.data.frame(afmat %>%
+    filter(phase %in% c('trunc1') & tpt != '23'))
+eec = as.data.frame(eec %>%
+    filter(phase %in% c('trunc1') & tpt != '23'))
+samps = afmat[,1:ncol(samps)]
+afmat = afmat[,-c(1:ncol(samps))]
+afmat = as.data.frame(t(afmat))
+eec = eec[,-c(1:ncol(samps))]
+eec = as.data.frame(t(eec))
+res = as.data.frame(fit_GLM_ContinuousTime(afMatrix = afmat ,rdMatrix = eec, vec = vec,
+                                               sampleData = samps, model.vars = 'tpt', poolCt=100, ncores = 16))
+df.glm = as.data.frame(cbind(df.glm, res))
+save(df.glm, file = paste0('./GLM/glm.Truncation1x2.RData'))
